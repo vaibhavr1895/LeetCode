e23b1a47e5fe9c91514598cd7217c080604d6d1b
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-02 19:28:35 +0530

    contains-duplicate

---
+++ contains-duplicate-217.cs
@@ -0,0 +1,10 @@
+public bool ContainsDuplicate(int[] nums) 
+{
+		HashSet<int> visited = new HashSet<int>();
+
+		foreach (int x in nums) { 
+			if(visited.Contains(x)) return true;
+			visited.Add(x);
+		}
+		return false;
+}
