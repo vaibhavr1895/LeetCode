22616b06a4dbfafddcdfdde053632e10f65752b8
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-03 21:18:38 +0530

    Added leetcode 11, container-with-most-water

---
+++ Arrays/container-with-most-water-11.cs
@@ -0,0 +1,18 @@
+public int MaxArea(int[] height) {
+        int start = 0;
+        int end = height.Length - 1;
+        int maxArea = 0;
+        while(start < end){
+            int width = end - start;
+            int lowestHeight = Math.Min(height[start], height[end]);
+            int curArea = width * lowestHeight;
+            maxArea = Math.Max(curArea, maxArea);
+            if(height[start] < height[end]){
+                start++;
+            }else{
+                end--;
+            }
+        }
+
+        return maxArea;
+    }
