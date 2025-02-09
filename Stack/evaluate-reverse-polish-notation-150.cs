fbc70d464a0ebad914ec1899770f7b3613ac014c
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-09 21:20:50 +0530

    Optimized the code for Leetcode 150

--- Stack/evaluate-reverse-polish-notation-150.cs
+++ Stack/evaluate-reverse-polish-notation-150.cs
@@ -2,47 +2,45 @@
 // O(n) space complexity
 public int EvalRPN(string[] tokens) {
 	Stack<int> nums = new Stack<int>();
-	string[] operators = new string[] { "+", "-", "*", "/" };
 	for (int i = 0; i < tokens.Length; i++)
 	{
-		if (!operators.Contains(tokens[i]))
+		if (!IsOperator(tokens[i]))
 		{
 			nums.Push(int.Parse(tokens[i]));
 		}
 		else
 		{
 			var op = tokens[i];
-			int[] curNums = new int[2];
-			int count = 1;
-			while (nums.Count() > 0 && count >= 0)
-			{
-				var n = nums.Pop();
-				curNums[count] = n;
-				count--;
-			}
-			int result = GetResult(curNums, op);
+			int num2 = nums.Pop();
+			int num1 = nums.Pop();
+			int result = GetResult(num1, num2, op);
 			nums.Push(result);
 		}
 	}
-	return nums.Count() > 0 ? nums.Pop() : 0;
+	return nums.Pop();
 }
 
-private int GetResult(int[] curNums, string op)
+private bool IsOperator(string token)
+{
+	return token == "+" || token == "-" || token == "*" || token == "/";
+}
+
+private int GetResult(int num1, int num2, string op)
 {
 	int result = 0;
 	switch (op)
 	{
 		case "+":
-			result = curNums[0] + curNums[1];
+			result = num1 + num2;
 			break;
 		case "-":
-			result = curNums[0] - curNums[1];
+			result = num1 - num2;
 			break;
 		case "*":
-			result = curNums[0] * curNums[1];
+			result = num1 * num2;
 			break;
 		case "/":
-			result = curNums[0] / curNums[1];
+			result = num1 / num2;
 			break;
 	}
 	return result;
