4069e380e018d1b0e5ad0095237094ac501ee2a4
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-18 21:54:59 +0530

    Solved leetcode 572, subtree-of-another-tree

---
+++ Binary Tree/subtree-of-another-tree-572.cs
@@ -0,0 +1,49 @@
+// O(n) time complexity
+// O(n) space complexity
+public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+	Queue<TreeNode> q = new Queue<TreeNode>();
+	
+	q.Enqueue(root);
+
+	while(q.Count() > 0){
+		var node = q.Dequeue();
+		if(node.val == subRoot.val){
+			var isSame = IsSameTree(node, subRoot);
+			if(isSame){
+				return true;
+			}
+		}
+
+		if(node.left != null){
+			q.Enqueue(node.left);
+		}
+		if(node.right != null){
+			q.Enqueue(node.right);
+		}
+	}        
+
+	return false;
+}
+
+private bool IsSameTree(TreeNode p, TreeNode q){
+	if(p == null && q == null){
+		return true;
+	}
+
+	if(p== null){
+		return false;
+	}
+
+	if(q == null){
+		return false;
+	}
+
+	if(p.val != q.val){
+		return false;
+	}
+
+	var left = IsSameTree(p.left, q.left);
+	var right = IsSameTree(p.right, q.right);
+
+	return left && right;
+}
