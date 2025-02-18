2933ebafc64d57b1b59a90205526044efcfb43d3
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-18 22:08:57 +0530

    Added optimized solution to leetcode 572

--- Binary Tree/subtree-of-another-tree-572.cs
+++ Binary Tree/subtree-of-another-tree-572.cs
@@ -1,5 +1,24 @@
-// O(n) time complexity
+// Difficulty: Easy
+// O(n * m) time complexity
 // O(n) space complexity
+public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+	if(subRoot == null){
+		return true;
+	}
+
+	if(root == null){
+		return false;
+	}
+
+	if(IsSameTree(root, subRoot)){
+		return true;
+	}
+
+	return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+}
+
+// O(s * t) time complexity
+// O(s) space complexity
 public bool IsSubtree(TreeNode root, TreeNode subRoot) {
 	Queue<TreeNode> q = new Queue<TreeNode>();
 	
