1e845b5566e9ea2450574ab7b4cca5d2de1b4d50
vaibhav1895 <vaibhavjrevankar@gmail.com>
2025-02-21 23:23:39 +0530

    Added one more solution for Kth Smallest Element in a BST

--- Binary Tree/kth-smallest-element-in-a-bst-230.cs
+++ Binary Tree/kth-smallest-element-in-a-bst-230.cs
@@ -4,6 +4,30 @@
 public int KthSmallest(TreeNode root, int k) {
 	Stack<TreeNode> s = new Stack<TreeNode>();
 
+	TreeNode cur = root;
+	int n = 0;
+	while(cur != null || s.Count > 0){
+		while(cur != null){
+			s.Push(cur);
+			cur = cur.left;
+		}
+
+		var node = s.Pop();
+		n++;
+
+		if(n == k){
+			return node.val;
+		}
+		if(node.right != null){
+			cur = node.right;
+		}
+	}
+	return -1;
+}
+	
+public int KthSmallest(TreeNode root, int k) {
+	Stack<TreeNode> s = new Stack<TreeNode>();
+
 	GetSorted(root, s);
 
 	int i = 0;
